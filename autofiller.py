5de6be2a0e00d615018d31d414b2658c2753a11c
marvin1099 <marvin1099@noreply.codeberg.org>
2024-09-21 20:42:21 +0000

    Add files for bitwarden autofiller

---
+++ autofiller.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+
+import clipboard
+import pyautogui
+import pywinctl
+import platform
+import psutil
+import os
+
+
+class AutoFiller:
+    def __init__(self, client, defaults):
+        self.sender = client.send_command
+        self.actions = defaults.fillactions
+        self.close = defaults.close
+        self.sync = defaults.sync
+
+    def get_active_process(self):
+        try:
+            # Get the focused window and retrieve the process name
+            active_window = pywinctl.getActiveWindow()
+            process_name = active_window.getAppName()
+
+            # Get a list of processes with matching names
+            # process_list = list(filter(lambda p: p.name() == process_name, psutil.process_iter()))
+            # process_list = None
+            if process_name:
+                process_info = {"window": active_window, "name": process_name}
+                return process_info
+            else:
+                print(f"Process is not set")
+                return None
+
+            # if process_list:
+            #     # Sort the process list by creation time (newest first)
+            #     sorted_processes = sorted(process_list, key=lambda p: p.create_time(), reverse=True)
+            #
+            #     # Get the newest process (most recent)
+            #     newest_process = sorted_processes[0]
+            #     process_info = {
+            #         'name': process_name,
+            #         'pid': newest_process.pid,
+            #         'cmdline': newest_process.cmdline(),
+            #         'status': newest_process.status()
+            #     }
+            #     return process_info
+
+        except Exception as e:
+            print(f"Unable to find process name for active window: {e}")
+            return None
+
+    def get_login_ids(self, process_name):
+        """Query Bitwarden for login item related to the process."""
+        if process_name:
+            query = f"pcprocess://{process_name}"
+            print(
+                f"Found active process, running query '{query}' to look for login item..."
+            )
+            response = self.sender(
+                "list",
+                ["items"],
+                dictfilter={"uriquery": query, "returns": "id"},
+            )
+            return response
+        else:
+            print(
+                f"With the process name {process_name} could not be determined."
+            )
+            return None
+
+    def get_login_item(self, item_id):
+        if item_id:
+            response = self.sender("get", ["item", item_id])
+            return response
+
+    def get_login_totp(self, item_id):
+        if item_id:
+            response = self.sender("get", ["totp", item_id])
+            return response
+
+    def accountselect(self, items, allactions):
+        activeaccount = 0  # The current selected account
+        lastacc = len(items)  # The number of available accounts
+        nractions = ""  # Buffer for collecting numeric actions
+        letters = None  # Current selected letter (account switcher)
+        haveyield = (
+            False  # Whether a value has been yielded in this iteration
+        )
+
+        if allactions:
+            for action in str(
+                allactions + "C"
+            ):  # Append 'C' to ensure selection execution
+                # Handle letters (account switching commands)
+                if action in "ABCD":
+                    if letters is None:
+                        letters = int(activeaccount)
+
+                    if action == "C":  # Select first account
+                        activeaccount = 0
+                    elif action == "D":  # Select last account
+                        activeaccount = lastacc - 1
+                    elif action == "A":  # Move to the next account
+                        activeaccount = (activeaccount + 1) % lastacc
+                    elif action == "B":  # Move to the previous account
+                        activeaccount = (
+                            activeaccount - 1 + lastacc
+                        ) % lastacc
+
+                    # Handle previously collected actions for the last account (if any)
+                    if letters is not None and nractions:
+                        yield items[letters], nractions
+                        haveyield = True
+                        letters = None
+                        nractions = ""
+
+                # Handle numeric actions (to be applied to the selected account)
+                elif action.isnumeric():
+                    nractions += action
+
+            if not haveyield:
+                yield None, None
+        else:
+            yield None, None
+
+    def autofill(self, login_id, actions):
+        item = self.get_login_item(login_id)
+        logindata = item.get("data", {}).get("login", {})
+        store = ""
+
+        usern = logindata.get("username")
+        if usern:
+            store = str(usern)
+        passw = logindata.get("password")
+        if passw and not store:
+            store = str(usern)
+        istotp = logindata.get("totp")
+
+        if actions:
+            for action in actions:
+                if action == "1":
+                    if usern:
+                        store = str(usern)
+                    else:
+                        store = ""
+                elif action == "2":
+                    if passw:
+                        store = str(passw)
+                    else:
+                        store = ""
+                elif action == "3":
+                    if istotp:
+                        store = str(
+                            self.get_login_totp(login_id)
+                            .get("data", {})
+                            .get("data", "")
+                        )
+                    else:
+                        store = ""
+                elif action == "4" and store:
+                    pyautogui.write(store)
+                elif action == "5" and store:
+                    clipboard.copy(store)
+                elif action == "6":
+                    pyautogui.press("enter")
+                else:
+                    pyautogui.press("tab")
+
+    def fill_process(self):
+        """Main function to perform autofill based on the active process."""
+        if self.close:
+            self.sender("exit")
+            print("Send exit command to deamon and now closing client...")
+            exit()
+        if self.sync:
+            sres = self.sender("sync")
+            if not sres.get("success"):
+                print(f"Problem syncing, skipping: {sres.get("message")}")
+            else:
+                print("Syncing successful")
+
+        process_info = self.get_active_process()
+        if process_info.get("name"):
+            process_name = process_info.get("name")
+
+            login_items = self.get_login_ids(process_name)
+            if login_items and login_items.get("success"):
+                print("Login item retrieved successfully.")
+                # Proceed to autofill (not implemented yet)
+                for item_id, actions in self.accountselect(
+                    login_items.get("returns", []), self.actions
+                ):
+                    print(item_id, actions)
+                    if item_id:
+                        self.autofill(item_id, actions)
+                    else:
+                        print("Actions not set or valid, closing...")
+            elif login_items.get("message") == "Not found.":
+                print("No login item with that process name, closing...")
+            else:
+                print("Failed to retrieve login item.")
+        else:
+            print("Could not determine the active process.")
---
+++ bitwardendamon.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+
+import multiprocessing
+import encryption
+import tempfile
+import socket
+import time
+import json
+import os
+
+
+class BitwardenDaemon(multiprocessing.Process):
+    def __init__(self, runner, defaults):
+        super().__init__()
+        self.session_key = None
+        self.runner = runner
+        self.timeout = defaults.timeout
+        self.host = defaults.host
+        self.port = defaults.port
+        self.encryption = defaults.encryption
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def set_session_key(self, key):
+        self.session_key = key
+
+    def run(self):
+        """Start the daemon process to listen for incoming requests."""
+        self.sock.bind((self.host, self.port))
+        self.sock.listen()
+        if self.timeout > -1:
+            self.sock.settimeout(self.timeout)
+        print(
+            f"Bitwarden CLI Daemon listening on {self.host}:{self.port}...\n"
+        )
+
+        while True:
+            try:
+                # Accept new connections
+                conn, addr = self.sock.accept()
+            except socket.timeout:  # if the timeout is reached exit
+                print("Socket waiting timeout reached\nExiting...")
+                try:
+                    self.sock.close()
+                except Exception as e:
+                    pass
+                exit()
+            except Exception as e:
+                print(
+                    f"Error occored while waiting for the connection: {e}\nExiting..."
+                )
+                try:
+                    self.sock.close()
+                except Exception as e:
+                    pass
+                exit()
+
+            with conn:
+                print(f"Connected by {addr}")
+
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+
+                    self.last_activity_time = time.time()
+
+                    saltfile = os.path.join(
+                        tempfile.gettempdir(),
+                        "Bitwarden+Cli-Autofiller+Script-Salt+File",
+                    )
+                    salt = ""
+                    if os.path.isfile(saltfile):
+                        with open(saltfile, "r") as salty:
+                            salt = salty.read()
+                    if len(salt) < 32:
+                        conn.close()
+                        print(
+                            "Error no salt file or short salt, listening too new connections...\n"
+                        )
+                        break
+
+                    # Encryption passphrase/key used for both encryption and decryption
+                    on_premise_key_local = "Bitwarden+Cli~Autofiller:Script-Comunication*Passphrase"
+                    on_premise_key_local += self.encryption
+                    on_premise_key_local += salt
+
+                    try:
+                        # Decrypt the received bytes (no need to decode)
+                        decrypted_data = encryption.decrypt_aes_256(
+                            data, on_premise_key_local
+                        )
+                    except Exception as e:
+                        print(
+                            f"Failed to decrypt incoming data (encryption password might not match): {e}\nlistening too new connections...\n"
+                        )
+                        conn.sendall("Decryption failed")
+                        conn.close()
+                        break
+
+                    # Process the decrypted request
+                    response = self.handle_request(decrypted_data)
+
+                    try:
+                        # Encrypt the outgoing response (already bytes)
+                        encrypted_response = encryption.encrypt_aes_256(
+                            json.dumps(response), on_premise_key_local
+                        )
+                    except Exception as e:
+                        print(
+                            f"Failed to encrypt response: {e}\nlistening too new connections...\n"
+                        )
+                        conn.sendall("Encryption failed")
+                        conn.close()
+                        break
+
+                    # Send encrypted response
+                    conn.sendall(encrypted_response)
+                    conn.close()
+
+                    # Remove salt file
+                    if os.path.isfile(saltfile):
+                        os.remove(saltfile)
+
+                    # exit()
+                    print(
+                        f"Finished connection, listening too new connections...\n"
+                    )
+                    break  # break to listen to new connections
+
+    def handle_request(self, request):
+        """Process the incoming request and run the appropriate Bitwarden command."""
+        try:
+            request_data = json.loads(request)
+            command = request_data.get("command", "")
+            args = request_data.get("args", [])
+            if isinstance(command, str) and command.lower() == "exit":
+                print("Daemon exit command issued, closing daemon...")
+                exit()
+            elif command:
+                # Append the session key to the command
+                argcp = list(args)
+                args.append("--session")
+                args.append(self.session_key)
+                result = self.runner.run_bw_command(
+                    [command] + args,
+                    dictfilter=request_data.get("filter", {}),
+                )
+                if result.get("success") != None:
+                    result.update({"command": [command] + argcp})
+                    return result
+                else:
+                    result.update(
+                        {
+                            "success": False,
+                            "command": [command] + argcp,
+                            "error": "No successful command",
+                        }
+                    )
+                    return result
+            else:
+                return {
+                    "success": False,
+                    "command": [command] + argcp,
+                    "error": "Invalid command",
+                }
+        except Exception as e:
+            return {
+                "success": False,
+                "command": [command] + argcp,
+                "error": str(e),
+            }
+
+    def is_running(self):
+        """Check if the daemon is already running by attempting to connect to the socket."""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            try:
+                sock.connect((self.host, self.port))
+                return True
+            except ConnectionRefusedError:
+                return False
+            except socket.error:
+                return False
